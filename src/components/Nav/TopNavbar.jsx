07aa8850a15b2a5a800fc8e85bd9820be175f53f
SDN33 <s.deinegri2@gmail.com>
2024-05-14 22:20:43 +0200

    presentation fini manque video bg

--- src/components/Nav/TopNavbar.jsx
+++ src/components/Nav/TopNavbar.jsx
@@ -88,12 +88,7 @@ const BurderWrapper = styled.button`
     display: block;
   }
 `;
-const UlWrapper = styled.ul`
-  display: flex;
-  @media (max-width: 760px) {
-    display: none;
-  }
-`;
+
 const UlWrapperRight = styled.ul`
   @media (max-width: 760px) {
     display: none;
