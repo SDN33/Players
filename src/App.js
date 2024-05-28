d1598792ca159e118d8475e271312b11c258b9c9
SDN33 <s.deinegri2@gmail.com>
2024-05-28 02:34:59 +0200

    tag seo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default function App() {
   return (
     <>
       <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
