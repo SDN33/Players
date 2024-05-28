3203dd216d3bdc26295441513b17fc0e28e3fba1
SDN33 <s.deinegri2@gmail.com>
2024-05-28 02:25:40 +0200

    tag seo

--- src/App.js
+++ src/App.js
@@ -11,17 +11,15 @@ export default function App() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
         <title>Players | Faire du sport n’a jamais été aussi challengeant </title>
-        <meta
-          name="description"
-          content="Rejoignez notre communauté dynamique de joueurs et vivez des expériences uniques.
-          Découvrez des partenaires de jeu, prêts à partager des moments inoubliables lors de matchs près de chez vous !
-          Que vous soyez un amateur de football, basketball ou d'un sport de raquette, vous trouverez votre place parmi nous.
-          Rencontrez de nouvelles personnes et dépassez-vous sur le terrain."
-        />
-        <meta
-          name="keywords"
-          content="football, basket-ball, tennis, padel, badminton, squash, sport, entraînement, compétition, équipe, match, club, athlète, jeunes, communauté, entraîneur, performance, technique, stratégie, formation, terrain, raquette, ballon, court, équipement, sportif, sports collectifs, sports individuels, loisirs sportifs"
-        />
+        <meta name="description" content="Rejoignez notre communauté sportive dynamique et trouvez des partenaires de jeu pour football, basketball, tennis, et plus encore. Vivez des expériences uniques près de chez vous !" />
+        <meta name="keywords" content="football, basket-ball, tennis, padel, badminton, squash, sport, entraînement, compétition, équipe, match, club, athlète, communauté, entraîneur, performance, stratégie, formation, terrain, raquette, ballon, court, équipement, sportif" />
+        <meta property="og:title" content="Rejoignez des Partenaires de Jeu | Sports Collectifs et Individuels" />
+        <meta property="og:description" content="Rejoignez notre communauté sportive dynamique et trouvez des partenaires de jeu pour football, basketball, tennis, et plus encore. Vivez des expériences uniques près de chez vous !" />
+        <meta property="og:image" content="https://res.cloudinary.com/daroyxenr/image/upload/c_thumb,w_200,g_face/v1715685571/Product_Features_Website_oum4io.svg" />
+        <meta property="og:url" content="https://players-pearl.vercel.app/" />
+        <meta name="twitter:title" content="Rejoignez des Partenaires de Jeu | Sports Collectifs et Individuels" />
+        <meta name="twitter:description" content="Rejoignez notre communauté sportive dynamique et trouvez des partenaires de jeu pour football, basketball, tennis, et plus encore. Vivez des expériences uniques près de chez vous !" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/daroyxenr/image/upload/c_thumb,w_200,g_face/v1715685571/Product_Features_Website_oum4io.svg" />
 
       </Helmet>
       <Landing />
