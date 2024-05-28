d5bd7ae940932a1b7781ff1d74df9416cf1e80be
SDN33 <s.deinegri2@gmail.com>
2024-05-28 02:38:41 +0200

    tag seo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-// Screens
 import Landing from "./screens/Landing.jsx";
 
 export default function App() {
   return (
-    <>
+    <div>
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
-        <title>Players | Faire du sport n’a jamais été aussi challengeant </title>
+        <title>Players | Faire du sport n’a jamais été aussi challengeant</title>
         <meta name="description" content="Rejoignez notre communauté sportive dynamique et trouvez des partenaires de jeu pour football, basketball, tennis, et plus encore. Vivez des expériences uniques près de chez vous !" />
         <meta name="keywords" content="football, basket-ball, tennis, padel, badminton, squash, sport, entraînement, compétition, équipe, match, club, athlète, communauté, entraîneur, performance, stratégie, formation, terrain, raquette, ballon, court, équipement, sportif" />
         <meta property="og:title" content="Rejoignez des Partenaires de Jeu | Sports Collectifs et Individuels" />
@@ -22,9 +21,8 @@ export default function App() {
         <meta name="twitter:title" content="Rejoignez des Partenaires de Jeu | Sports Collectifs et Individuels" />
         <meta name="twitter:description" content="Rejoignez notre communauté sportive dynamique et trouvez des partenaires de jeu pour football, basketball, tennis, et plus encore. Vivez des expériences uniques près de chez vous !" />
         <meta name="twitter:image" content="https://res.cloudinary.com/daroyxenr/image/upload/c_thumb,w_200,g_face/v1715685571/Product_Features_Website_oum4io.svg" />
-
       </Helmet>
       <Landing />
-    </>
+    </div>
   );
 }
